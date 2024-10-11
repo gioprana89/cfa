4ad5f97c5c7777082796659e7a78da487c78e4af
gioprana89 <gioprana89@gmail.com>
2024-10-11 08:05:02 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -103,6 +103,149 @@ if pilih_topik == ":rainbow[Confirmatory Factor Analysis (CFA) dengan JAMOVI]":
 
 
 
+if pilih_topik == ":rainbow[Confirmatory Factor Analysis (CFA) dengan LISREL]":
+    st.write('''<br><br><br><center><font color = "red" size = 7>2018</font></center> ''', unsafe_allow_html = True)
+
+    st.write('''[1] <font color = "#0000ff">Judul: PENGUJIAN VALIDITAS DAN RELIABILITAS KONSTRUK PADA ORGANIZATIONAL CITIZENSHIP BEHAVIOR</font><br><font color = "#ff00ff">Jurnal: HUMANITAS</font><br><font color = "#f4c430">Tahun Terbit Artikel: 2018</font><br><font color = "#32cd32">Publisher: </font><br><a href = "https://www.proquest.com/docview/2656292561?pq-origsite=gscholar&fromopenview=true&sourcetype=Scholarly%20Journals" target = "_blank" style = "text-decoration:none">Link Artikel</a>
+
+
+
+             ''', unsafe_allow_html = True)
+    
+
+    st.markdown(
+    """<a href="https://www.proquest.com/docview/2656292561?pq-origsite=gscholar&fromopenview=true&sourcetype=Scholarly%20Journals">
+    <img src="https://statkomat.com/aplikasi-cfa/lisrel/1/1.png" width="600"><br>
+    <img src="https://statkomat.com/aplikasi-cfa/lisrel/1/2.png" width="600"><br>
+    <img src="https://statkomat.com/aplikasi-cfa/lisrel/1/3.png" width="600"><br>
+    <img src="https://statkomat.com/aplikasi-cfa/lisrel/1/4.png" width="600">
+    </a>""",
+    unsafe_allow_html=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if pilih_topik == ":rainbow[Confirmatory Factor Analysis (CFA) dengan AMOS]":
+    st.write('''<br><br><br><center><font color = "red" size = 7>2022</font></center> ''', unsafe_allow_html = True)
+
+    st.write('''[1] <font color = "#0000ff">Judul: KESAHAN DAN KEBOLEHPERCAYAAN INVENTORI KEYAKINAN SUKAN: PENGGUNAAN MODEL RASCH DAN AMOS</font><br><font color = "#ff00ff">Jurnal: Journal of Educational Research</font><br><font color = "#f4c430">Tahun Terbit Artikel: 2022</font><br><font color = "#32cd32">Publisher: </font><br><a href = "https://borneojournal.um.edu.my/index.php/JER/article/view/41267" target = "_blank" style = "text-decoration:none">Link Artikel</a>
+
+
+
+             ''', unsafe_allow_html = True)
+    
+
+    st.markdown(
+    """<a href="https://borneojournal.um.edu.my/index.php/JER/article/view/41267">
+    <img src="https://statkomat.com/aplikasi-cfa/amos/3/1.png" width="700"><br>
+    <img src="https://statkomat.com/aplikasi-cfa/amos/3/2.png" width="600"><br>
+    <img src="https://statkomat.com/aplikasi-cfa/amos/3/3.png" width="600"><br>
+    </a>""",
+    unsafe_allow_html=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    st.write('''<br><br><br><center><font color = "red" size = 7>2019</font></center> ''', unsafe_allow_html = True)
+
+    st.write('''[1] <font color = "#0000ff">Judul: Pengesahan Model Kajian Menggunakan Analisis Faktor Kesahan (AFK) Bagi Konstruk Pengetahuan Agama, Kaedah Penyampaian, Kecerdasan Emosi dan Peningkatan Amal Ibadah dalam Kalangan Komuniti Masjid</font><br><font color = "#ff00ff">Jurnal: International Journal of Civilizational Studies and Human Sciences</font><br><font color = "#f4c430">Tahun Terbit Artikel: 2018</font><br><font color = "#32cd32">Publisher: </font><br><a href = "https://www.bitarajournal.com/index.php/bitarajournal/article/view/47" target = "_blank" style = "text-decoration:none">Link Artikel</a>
+
+
+
+             ''', unsafe_allow_html = True)
+    
+
+    st.markdown(
+    """<a href="https://www.bitarajournal.com/index.php/bitarajournal/article/view/47">
+    <img src="https://statkomat.com/aplikasi-cfa/amos/1/1.png" width="400"><br>
+    <img src="https://statkomat.com/aplikasi-cfa/amos/1/2.png" width="600"><br>
+    <img src="https://statkomat.com/aplikasi-cfa/amos/1/3.png" width="600"><br>
+    <img src="https://statkomat.com/aplikasi-cfa/amos/1/4.png" width="600"><br>
+    <img src="https://statkomat.com/aplikasi-cfa/amos/1/5.png" width="600">
+    </a>""",
+    unsafe_allow_html=True)
+
+
+
+
+
+
+    st.write('''<br><br><br><center><font color = "red" size = 7>2013</font></center> ''', unsafe_allow_html = True)
+
+    st.write('''[1] <font color = "#0000ff">Judul: Model Persamaan Struktur untuk Mengkaji Penggunaan Kenderaan tidak Bermotor Berdasarkan Teori Tingkah Laku Terancang</font><br><font color = "#ff00ff">Jurnal: Jurnal Teknologi</font><br><font color = "#f4c430">Tahun Terbit Artikel: 2013</font><br><font color = "#32cd32">Publisher: </font><br><a href = "https://journals.utm.my/jurnalteknologi/article/view/1341" target = "_blank" style = "text-decoration:none">Link Artikel</a>
+
+
+
+             ''', unsafe_allow_html = True)
+    
+
+    st.markdown(
+    """<a href="https://journals.utm.my/jurnalteknologi/article/view/1341">
+    <img src="https://statkomat.com/aplikasi-cfa/amos/2/1.png" width="400"><br>
+    <img src="https://statkomat.com/aplikasi-cfa/amos/2/2.png" width="600"><br>
+    <img src="https://statkomat.com/aplikasi-cfa/amos/2/3.png" width="600"><br>
+    <img src="https://statkomat.com/aplikasi-cfa/amos/2/4.png" width="600"><br>
+    <img src="https://statkomat.com/aplikasi-cfa/amos/2/5.png" width="600">
+    </a>""",
+    unsafe_allow_html=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 st.markdown("")
 st.markdown("")
 st.markdown("")
